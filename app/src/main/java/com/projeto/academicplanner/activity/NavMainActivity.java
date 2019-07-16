3898434ea34d951c27b6c23df80c0951c5664d38
Breno Garcia <breno.garcia@gmail.com>
2019-07-16 11:17:16 +0100

    menu is done

--- app/src/main/java/com/projeto/academicplanner/activity/NavMainActivity.java
+++ app/src/main/java/com/projeto/academicplanner/activity/NavMainActivity.java
@@ -153,9 +153,7 @@ public class NavMainActivity extends AppCompatActivity
 
         if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_events) {
 
         } else if (id == R.id.nav_settings) {
 
