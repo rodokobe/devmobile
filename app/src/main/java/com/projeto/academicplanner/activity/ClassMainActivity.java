6446012802a1feb61bf3437c804705be77132b54
Breno Garcia <breno.garcia@gmail.com>
2019-07-22 21:41:23 +0100

    Send Email

--- app/src/main/java/com/projeto/academicplanner/activity/ClassMainActivity.java
+++ app/src/main/java/com/projeto/academicplanner/activity/ClassMainActivity.java
@@ -1,19 +1,53 @@
 package com.projeto.academicplanner.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.projeto.academicplanner.Interface.IFirebaseLoadDoneClasses;
+import com.projeto.academicplanner.Interface.IFirebaseLoadDoneCourse;
+import com.projeto.academicplanner.Interface.IFirebaseLoadDoneDiscipline;
+import com.projeto.academicplanner.Interface.IFirebaseLoadDoneUniversity;
+import com.projeto.academicplanner.Interface.IFirebaseLoadDoneYears;
 import com.projeto.academicplanner.R;
 import com.projeto.academicplanner.fragment.ClassAddFragment;
 import com.projeto.academicplanner.fragment.ClassUpdateFragment;
+import com.projeto.academicplanner.helper.ConfigFirebase;
+import com.projeto.academicplanner.model.Course;
+import com.projeto.academicplanner.model.University;
+import com.toptoche.searchablespinnerlibrary.SearchableSpinner;
 
-public class ClassMainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClassMainActivity extends AppCompatActivity
+        /*implements IFirebaseLoadDoneYears, IFirebaseLoadDoneUniversity,
+        IFirebaseLoadDoneCourse, IFirebaseLoadDoneDiscipline, IFirebaseLoadDoneClasses*/ {
 
     private ClassAddFragment classAddFragment;
     private ClassUpdateFragment classUpdateFragment;
+
+    String idUniversitySelectedU, nameUniversitySelectedU;
+    String idCourseSelectedC, nameCourseSelectedC;
+
+
+    private String idUserLogged;
+    private SearchableSpinner spinnerYears, spinnerUniversities, spinnerCourses, spinnerDisciplines;
+
+    private IFirebaseLoadDoneYears iFirebaseLoadDoneYears;
+    private IFirebaseLoadDoneUniversity iFirebaseLoadDoneUniversity;
+    private IFirebaseLoadDoneCourse iFirebaseLoadDoneCourse;
+    private IFirebaseLoadDoneDiscipline iFirebaseLoadDoneDiscipline;
+    private IFirebaseLoadDoneClasses iFirebaseLoadDoneClasses;
+
+    private DatabaseReference yearsRef, universitiesRef, coursesRef, disciplinesRef, classesRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +58,8 @@ public class ClassMainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Add New Class");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        initializingComponents();
 
         addClassFragment();
 
@@ -42,4 +78,96 @@ public class ClassMainActivity extends AppCompatActivity {
         transaction.replace(R.id.frameClassMain, classUpdateFragment);
         transaction.commit();
     }
+
+   /* @Override
+    public void onFireBaseLoadUniversitySuccess(final List<University> universitiesList) {
+
+        //universitySpinner = universitiesList;
+        final List<String> university_name = new ArrayList<>();
+        for (University university : universitiesList)
+
+            university_name.add(university.getUniversityName());
+
+        //Create adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, university_name);
+        spinnerUniversities.setAdapter(adapter);
+
+        spinnerUniversities.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                idUniversitySelectedU = universitiesList.get(position).getIdUniversity();
+                nameUniversitySelectedU = universitiesList.get(position).getUniversityName();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+    @Override
+    public void onFireBaseLoadUniversityFailed(String message) {
+    }
+
+    //charge the spinner Course values
+    @Override
+    public void onFireBaseLoadCourseSuccess(final List<Course> coursesList) {
+
+        //universitySpinner = universitiesList;
+        final List<String> university_name = new ArrayList<>();
+        for (Course course : coursesList)
+
+            university_name.add(course.getUniversityName() + "\n" + course.getCourseName());
+
+        //Create adapter
+        ArrayAdapter<String> adapterUniversity = new ArrayAdapter<>( this, android.R.layout.simple_list_item_1, university_name);
+        spinnerCourses.setAdapter(adapterUniversity);
+
+        spinnerCourses.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                idCourseSelectedC = coursesList.get(position).getIdCourse();
+                nameCourseSelectedC = coursesList.get(position).getCourseName();
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+    }
+
+    @Override
+    public void onFireBaseLoadCourseFailed(String message) {
+    }
+*/
+
+    private void initializingComponents(){
+
+        spinnerYears = findViewById(R.id.spinnerYears);
+        spinnerUniversities = findViewById(R.id.spinnerUniversities);
+        spinnerCourses = findViewById(R.id.spinnerCourses);
+        spinnerDisciplines = findViewById(R.id.spinnerDisciplines);
+
+        idUserLogged = ConfigFirebase.getUserId();
+
+        yearsRef = FirebaseDatabase.getInstance().getReference("years").child(idUserLogged);
+        universitiesRef = FirebaseDatabase.getInstance().getReference("universities").child(idUserLogged);
+        coursesRef = FirebaseDatabase.getInstance().getReference("courses").child(idUserLogged);
+        disciplinesRef = FirebaseDatabase.getInstance().getReference("disciplines").child(idUserLogged);
+        classesRef = FirebaseDatabase.getInstance().getReference("disciplines").child(idUserLogged);
+
+
+       /* iFirebaseLoadDoneYears = this;
+        iFirebaseLoadDoneUniversity = this;
+        iFirebaseLoadDoneCourse = this;
+        iFirebaseLoadDoneDiscipline = this;
+        iFirebaseLoadDoneClasses = this;*/
+
+    }
+
 }
--- app/src/main/java/com/projeto/academicplanner/activity/SendEmailActivity.java
+++ app/src/main/java/com/projeto/academicplanner/activity/SendEmailActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,6 +19,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.projeto.academicplanner.Interface.IFirebaseLoadDoneDiscipline;
 import com.projeto.academicplanner.R;
@@ -49,7 +51,7 @@ public class SendEmailActivity extends AppCompatActivity implements IFirebaseLoa
     private DatabaseReference databaseDisciplineReference;
     private IFirebaseLoadDoneDiscipline iFirebaseLoadDoneDiscipline;
 
-    private final List<String> allStudentsEmailList = new ArrayList<>();
+    private List<String> allStudentsEmailList = new ArrayList<>();
     private final List<String> allDelegatesEmailList = new ArrayList<>();
 
     //Send button
@@ -95,45 +97,29 @@ public class SendEmailActivity extends AppCompatActivity implements IFirebaseLoa
 
         buttonSend.setOnClickListener(view -> {
             if (switchStdOrDlgt.isChecked()) {
-                delegatesEmail();
+                delegatesEmail(idDisciplineSelected);
             } else {
-                allStudentsEmail();
+                allStudentsEmail(idDisciplineSelected);
             }
         });
     }
 
-    public void allStudentsEmail() {
+    public void allStudentsEmail(String idDisciplineSelected) {
 
-        DatabaseReference disciplineRef = databaseDisciplineReference
-                .child("disciplines")
-                .child(idUserLogged);
+        DatabaseReference students = databaseDisciplineReference
+                .child(idDisciplineSelected)
+                .child("students");
 
-        disciplineRef.addValueEventListener(new ValueEventListener() {
+        students.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    Discipline discipline = snapshot.getValue(Discipline.class);
-                    idDiscipline = discipline.getIdDiscipline();
+                for (DataSnapshot snap: dataSnapshot.getChildren()){
+                    Student student = snap.getValue(Student.class);
 
-                    DatabaseReference studentsInDisciplineRef = disciplineRef.child("students");
+                    String studentEmail = student.getStudentEmail();
 
-                    studentsInDisciplineRef.addValueEventListener(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                            for (DataSnapshot snap : dataSnapshot.getChildren()) {
-                                Student student = snap.getValue(Student.class);
-
-                                String studentEmail = student.getStudentEmail();
-                                sendEmail(studentEmail);
-
-                            }
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                        }
-                    });
+                    Log.i("Emails: ", studentEmail);
+                    sendEmail(studentEmail);
 
                 }
             }
@@ -145,41 +131,23 @@ public class SendEmailActivity extends AppCompatActivity implements IFirebaseLoa
         });
     }
 
-    public void delegatesEmail() {
+    public void delegatesEmail(String idDisciplineSelected) {
 
-        DatabaseReference disciplineRef = databaseDisciplineReference
-                .child("disciplines")
-                .child(idUserLogged);
+        DatabaseReference students = databaseDisciplineReference
+                .child(idDisciplineSelected)
+                .child("students");
 
-        disciplineRef.addValueEventListener(new ValueEventListener() {
+        students.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    Discipline discipline = snapshot.getValue(Discipline.class);
-                    idDiscipline = discipline.getIdDiscipline();
+                for (DataSnapshot snap: dataSnapshot.getChildren()){
+                    Student student = snap.getValue(Student.class);
 
-                    DatabaseReference studentsInDisciplineRef = disciplineRef.child("students");
+                    String studentEmail = student.getStudentEmail();
 
-                    studentsInDisciplineRef.addValueEventListener(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                            for (DataSnapshot snap : dataSnapshot.getChildren()) {
-                                Student student = snap.getValue(Student.class);
-
-                                String studentEmail = student.getStudentEmail();
-
-                                if (student.getStudentDelegate().equals("YES")) {
-                                    allDelegatesEmailList.add(studentEmail);
-                                    sendEmail(studentEmail);
-                                }
-                            }
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                        }
-                    });
+                    if (student.getStudentDelegate().equals("YES")) {
+                        sendEmail(studentEmail);
+                    }
 
                 }
             }
@@ -193,76 +161,77 @@ public class SendEmailActivity extends AppCompatActivity implements IFirebaseLoa
     }
 
 
-    //charge the spinner Discipline values
-    @Override
-    public void onFireBaseLoadDisciplineSuccess(final List<Discipline> disciplinesList) {
+        //charge the spinner Discipline values
+        @Override
+        public void onFireBaseLoadDisciplineSuccess ( final List<Discipline> disciplinesList){
 
-        final List<String> discipline_info = new ArrayList<>();
-        for (Discipline discipline : disciplinesList)
+            final List<String> discipline_info = new ArrayList<>();
+            for (Discipline discipline : disciplinesList)
 
-            discipline_info.add(discipline.getDisciplineName() + "\n"
-                    + discipline.getDisciplineYearName() + "   Semester: " + discipline.getDisciplineSemester() + "\n"
-                    + discipline.getCourseName() + "\n"
-                    + discipline.getUniversityName());
+                discipline_info.add(discipline.getDisciplineName() + "\n"
+                        + discipline.getDisciplineYearName() + "   Semester: " + discipline.getDisciplineSemester() + "\n"
+                        + discipline.getCourseName() + "\n"
+                        + discipline.getUniversityName());
 
-        /**
-         * Creates adapter
-         */
-        ArrayAdapter<String> adapterDiscipline = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, discipline_info);
-        spinnerDisciplines.setAdapter(adapterDiscipline);
+            /**
+             * Creates adapter
+             */
+            ArrayAdapter<String> adapterDiscipline = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, discipline_info);
+            spinnerDisciplines.setAdapter(adapterDiscipline);
 
-        spinnerDisciplines.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            spinnerDisciplines.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-                idUniversitySelected = disciplinesList.get(position).getIdUniversity();
-                nameUniversitySelected = disciplinesList.get(position).getUniversityName();
-                idCourseSelected = disciplinesList.get(position).getIdCourse();
-                nameCourseSelected = disciplinesList.get(position).getCourseName();
-                idDisciplineSelected = disciplinesList.get(position).getIdDiscipline();
-                nameDisciplineSelected = disciplinesList.get(position).getDisciplineName();
-                idYearSelected = disciplinesList.get(position).getDisciplineYearId();
-                nameYearSelected = disciplinesList.get(position).getDisciplineYearName();
-                semester = disciplinesList.get(position).getDisciplineSemester();
-            }
+                    idUniversitySelected = disciplinesList.get(position).getIdUniversity();
+                    nameUniversitySelected = disciplinesList.get(position).getUniversityName();
+                    idCourseSelected = disciplinesList.get(position).getIdCourse();
+                    nameCourseSelected = disciplinesList.get(position).getCourseName();
+                    idDisciplineSelected = disciplinesList.get(position).getIdDiscipline();
+                    nameDisciplineSelected = disciplinesList.get(position).getDisciplineName();
+                    idYearSelected = disciplinesList.get(position).getDisciplineYearId();
+                    nameYearSelected = disciplinesList.get(position).getDisciplineYearName();
+                    semester = disciplinesList.get(position).getDisciplineSemester();
+                }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+                }
+            });
+
+        }
+
+        @Override
+        public void onFireBaseLoadDisciplineFailed (String message){
+        }
+
+
+        private void sendEmail (String sendTo) {
+            //Getting content for email
+
+            String email = sendTo;
+            String subject = editTextSubject.getText().toString().trim();
+            String message = editTextMessage.getText().toString().trim();
+
+            //Creating SendMail object
+            SendMail sm = new SendMail(this, email, subject, message);
+
+            //Executing sendmail to send email
+            sm.execute();
+        }
+
+        private void initializingComponents () {
+
+            editTextSubject = findViewById(R.id.editTextSubject);
+            editTextMessage = findViewById(R.id.editTextMessage);
+            switchStdOrDlgt = findViewById(R.id.switchStdOrDlgt);
+            buttonSend = findViewById(R.id.buttonSend);
+            spinnerDisciplines = findViewById(R.id.spinnerDisciplines);
+
+            idUserLogged = ConfigFirebase.getUserId();
+            databaseDisciplineReference = FirebaseDatabase.getInstance().getReference("disciplines").child(idUserLogged);
+            iFirebaseLoadDoneDiscipline = this;
+
+        }
 
     }
-
-    @Override
-    public void onFireBaseLoadDisciplineFailed(String message) {
-    }
-
-
-    private void sendEmail(String sendTo) {
-        //Getting content for email
-        String email = sendTo.trim();
-        String subject = editTextSubject.getText().toString().trim();
-        String message = editTextMessage.getText().toString().trim();
-
-        //Creating SendMail object
-        SendMail sm = new SendMail(this, email, subject, message);
-
-        //Executing sendmail to send email
-        sm.execute();
-    }
-
-    private void initializingComponents() {
-
-        editTextSubject = findViewById(R.id.editTextSubject);
-        editTextMessage = findViewById(R.id.editTextMessage);
-        switchStdOrDlgt = findViewById(R.id.switchStdOrDlgt);
-        buttonSend = findViewById(R.id.buttonSend);
-        spinnerDisciplines = findViewById(R.id.spinnerDisciplines);
-
-        idUserLogged = ConfigFirebase.getUserId();
-        databaseDisciplineReference = FirebaseDatabase.getInstance().getReference("disciplines").child(idUserLogged);
-        iFirebaseLoadDoneDiscipline = this;
-
-    }
-
-}
